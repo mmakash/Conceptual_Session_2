c733caf1cfe3b9f96da1a2181d9c763425fd4c90
mmakash <musaddekurakash@gmail.com>
2023-07-25 11:15:31 +0600

    name updated

--- main.js
+++ main.js
@@ -4,4 +4,8 @@ console.log("I love Bangladesh");
 let x = 15;
 let y = 10;
 let z = x + y;
+let firstName = "Ashish";
+let lastName = "Nehra";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
 console.log(z);
